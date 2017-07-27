507f15a439de454253c8ff4c1727f1a271f32e62
AlbinoDrought <albinodrought@gmail.com>
2017-07-27 08:31:39 -0700

    Attempt to stop the webview from opening the external browser, and enable javascript

--- app/src/main/java/party/minge/reddit/ExternalWebResourceActivity.java
+++ app/src/main/java/party/minge/reddit/ExternalWebResourceActivity.java
@@ -1,7 +1,10 @@
 package party.minge.reddit;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -19,7 +22,18 @@ public class ExternalWebResourceActivity extends Activity {
 
     @AfterViews
     @UiThread
+    @SuppressLint("SetJavaScriptEnabled")
     protected void visitUrl() {
+        this.webview.setWebChromeClient(new WebChromeClient());
+
+        // the default webview client will occasionally open the browser
+        // we don't want this - we want content to be displayed,
+        // by default, in the app.
+        this.webview.setWebViewClient(new WebViewClient());
+
+        // allow js
+        this.webview.getSettings().setJavaScriptEnabled(true);
+
         this.webview.loadUrl(this.url);
     }
 }
